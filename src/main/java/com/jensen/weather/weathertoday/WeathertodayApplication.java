2b8334f279c49edf2874a59a9665a65cf8d2195e
lakshmi5382 <v.l.bharadwaja@gmail.com>
2024-01-30 09:44:38 +0100

    InitialCommit

---
+++ src/main/java/com/jensen/weather/weathertoday/WeathertodayApplication.java
@@ -0,0 +1,13 @@
+package com.jensen.weather.weathertoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathertodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathertodayApplication.class, args);
+	}
+
+}
